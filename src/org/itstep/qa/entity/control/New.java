f7917c4367be9620224456b989a42cf0f23e9c9e
qa <xbx@jn.dd>
2018-08-18 19:26:28 +0300

    added new class

--- src/org/itstep/qa/entity/control/New.java
+++ src/org/itstep/qa/entity/control/New.java
@@ -1,12 +1,13 @@
+
 package org.itstep.qa.entity.control;
 
 public class TestOne {
     public static void main(String[] args) { // индификатор доступа(public static void) Имя(main) все вместе это обьявление метода , а между фигурными скобками лежит его реализация.
         int testVar;  // переменная объявлена
-            testVar = 3;
-            int testTwo;
-            testTwo =(2 + testVar) * 3 ;
-            System.out.println(testTwo);
+        testVar = 3;
+        int testTwo;
+        testTwo =(2 + testVar) * 3 ;
+        System.out.println(testTwo);
 
     }
 }
